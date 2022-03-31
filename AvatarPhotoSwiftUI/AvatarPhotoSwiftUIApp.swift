aa4aa72ad74f951ef54bcfd70fb94a88ebf7c14a
Alexander Panin <panin1970@gmail.com>
2022-03-31 11:19:30 +0300

    Initial Commit

---
+++ AvatarPhotoSwiftUI/AvatarPhotoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvatarPhotoSwiftUIApp.swift
+//  AvatarPhotoSwiftUI
+//
+//  Created by Александр Панин on 31.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AvatarPhotoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
